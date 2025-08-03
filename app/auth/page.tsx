81ad230be3738185cbe8de1aab4b2d8131e971ab
ayush-0012 <ayushbisht0012@gmail.com>
2025-08-03 14:52:51 +0530

    user signIn using better auth client

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -6,9 +8,25 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { authClient } from "@/lib/auth-client";
 import { Github, Chrome } from "lucide-react";
 
 export default function AuthPage() {
+  // github auth handler
+  async function handleGithubAuth() {
+    try {
+      const { data, error } = await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/dashboard",
+      });
+
+      console.log("data", data);
+      console.log("error", error);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50 dark:bg-gray-950 p-4">
       <Card className="w-full max-w-md rounded-lg shadow-lg">
@@ -21,7 +39,10 @@ export default function AuthPage() {
           </CardDescription>
         </CardHeader>
         <CardContent className="grid gap-4">
-          <Button className="w-full bg-gradient-to-br from-gray-800 via-gray-900 to-black text-white shadow-lg hover:from-gray-700 hover:via-gray-800 hover:to-gray-900 transition-all duration-200 relative overflow-hidden group">
+          <Button
+            className="w-full bg-gradient-to-br from-gray-800 via-gray-900 to-black text-white shadow-lg hover:from-gray-700 hover:via-gray-800 hover:to-gray-900 transition-all duration-200 relative overflow-hidden group"
+            onClick={handleGithubAuth}
+          >
             <span className="absolute inset-0 bg-gradient-to-br from-purple-600 via-pink-500 to-red-500 opacity-0 group-hover:opacity-20 transition-opacity duration-300 blur-sm"></span>
             <Github className="mr-2 h-5 w-5" />
             Sign in with GitHub
