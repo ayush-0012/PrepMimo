58eee14348f63dd034aa6908d51c7a181cb759b0
ayush-0012 <ayushbisht0012@gmail.com>
2025-08-18 13:40:42 +0530

    loading env according to environment

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.NEXT_PUBLIC_PROD_URL || process.env.BETTER_AUTH_URL,
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_PROD_URL
+      : process.env.BETTER_AUTH_URL,
 });
