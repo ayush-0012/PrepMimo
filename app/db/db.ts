e3bb724bd8b2393f781c8051a56c99199f9c82b6
ayush-0012 <ayushbisht0012@gmail.com>
2025-09-10 14:09:15 +0530

    made a check before using the db url

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,5 +1,10 @@
 import { drizzle } from "drizzle-orm/neon-http";
 
+if (!process.env.DATABASE_URL) {
+  console.log("db env", process.env.DATABASE_URL);
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
 const db = drizzle(process.env.DATABASE_URL!);
 
 export default db;
